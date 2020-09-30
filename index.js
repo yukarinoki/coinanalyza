786296f8940921a75072466d779c66d57ea83584
yukarinoki <blkheartp@gmail.com>
2020-09-30 12:32:01 +0000

    add c3.js(d3.js)

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
+  .use("/c3", express.static(__dirname + "/node_modules/c3"))
+  .use("/d3", express.static(__dirname + "/node_modules/d3"))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
