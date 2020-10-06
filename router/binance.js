1f0f7eb315c42017f6a988c47ecebc8e871a6c3f
yukarinoki <yukarinoki@naopc.local>
2020-10-06 11:36:51 +0900

    [fix] name of environment var

--- router/binance.js
+++ router/binance.js
@@ -7,7 +7,7 @@ console.log(process.env.ENV_DB)
 
 var pool = new pg.Pool({
     host: process.env.ENV_HOST,
-    database: process.env.ENV_DB,
+    database: process.env.ENV_DATABASE,
     user: process.env.ENV_USER,
     port: 5432,
     password: process.env.ENV_PASSWORD,
